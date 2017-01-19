34ba7bfd6e6993ad4cd771230768e793bc62fb76
Alex Kalinkin <alex@kalinkin.info>
2017-01-19 11:41:56 +0300

    Add localisation structure to project

--- app.desktop/app.py
+++ app.desktop/app.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 import os
 import signal
+import locale
+import gettext
+from gui.window_manager import WindowManager
 
 __author__ = "Alex Kalinkin"
 
 
 APP_INDICATOR_ID = 'genki.app.desktop'
 APP_ICON = 'assets/app_icon.svg'
-APP_WINDOW_MANAGER = 'Qt5'  # ['Qt5','Gtk3']
+APP_WINDOW_MANAGER = 'Gtk3'  # ['Qt5','Gtk3']
 AVAILABLE_L10NS = ['ru_RU', 'en_US']
 DEFAULT_L10N = 'en_US'
 
@@ -18,6 +21,30 @@ def main():
 
 def init():
     set_work_directory()
+    translation = init_l10n()
+    window_manager = WindowManager(APP_WINDOW_MANAGER, translation)
+    print('Application started with AppId: "{0}"'.format(APP_INDICATOR_ID))
+    window_manager.start(APP_INDICATOR_ID, APP_ICON)
+
+
+def init_l10n():
+    print('Available translations: {0}'.format(AVAILABLE_L10NS))
+
+    current_locale = locale.getdefaultlocale()
+    print('Current l10n: {0}'.format(current_locale))
+
+    if current_locale[0] in AVAILABLE_L10NS:
+        locale_code = current_locale[0]
+    else:
+        locale_code = DEFAULT_L10N
+
+    lang = gettext.translation('app', localedir='l10n', languages=[locale_code])
+    lang.install()
+
+    from l10n.app_strings import Translations
+    t = Translations
+
+    return t
 
 
 def set_work_directory():
